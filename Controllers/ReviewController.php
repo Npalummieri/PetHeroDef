ab3cda0544d6ab929cd995ff9bf757e43c302685
Npalummieri <palummierinicolas@gmail.com>
2024-03-28 13:55:05 -0300

    review changes (delete&errorhandling)

--- Controllers/ReviewController.php
+++ Controllers/ReviewController.php
@@ -1,57 +1,51 @@
-<?php 
+<?php
 
 namespace Controllers;
 
 
 
-use DAO\ReviewDAO as ReviewDAO;
+
 use Services\ReviewService as ReviewService;
 use Utils\Session as Session;
-use Controllers\KeeperController as KeeperController;
 
- 
-class ReviewController{
+
+
+class ReviewController
+{
 
 
     private $reviewService;
-    private $keeperController;
+
 
     public function __construct()
     {
 
         $this->reviewService = new ReviewService();
-        $this->keeperController = new KeeperController();
     }
 
-    //Te envia al form de review (vista)
-    public function doReview($keeperCode,$comment,$score)
+    public function doReview($keeperCode, $comment, $score)
     {
-        
-        echo " SOY POST ";
-        var_dump($_POST);
         if (Session::IsLogged()) {
             if (Session::GetTypeLogged() == "Models\Owner") {
                 $loggedUser = Session::GetLoggedUser();
-                $canReview = $this->reviewService->srv_canReview($loggedUser->getOwnerCode(),$keeperCode);
-                if($canReview == 1)
-                {
+                $canReview = $this->reviewService->srv_canReview($loggedUser->getOwnerCode(), $keeperCode);
+                if ($canReview == 1) {
                     echo "ENTRE AL GMSGE";
-                    $this->reviewService->srv_add($loggedUser->getOwnerCode(),$keeperCode,$comment,$score);
+                    $this->reviewService->srv_add($loggedUser->getOwnerCode(), $keeperCode, $comment, $score);
                     Session::SetOkMessage("Review added successfully!");
-                    //header("location: ".FRONT_ROOT."Keeper/showProfileKeeper/".$keeperCode);
-                }else{
+                    header("location: " . FRONT_ROOT . "Keeper/showProfileKeeper/" . $keeperCode);
+                } else {
                     Session::SetBadMessage("Error making the review.You must have a FINISHED booking with this keeper <br> <strong>Note</strong> : If you have it.Can't review it more than 3 times ");
-                    //header("location: ".FRONT_ROOT."Keeper/showProfileKeeper/".$keeperCode);
+                    header("location: " . FRONT_ROOT . "Keeper/showProfileKeeper/" . $keeperCode);
                 }
             } else {
-                header("location: ".FRONT_ROOT."Home/showLoginView");
+                header("location: " . FRONT_ROOT . "Home/showLoginView");
                 Session::DeleteSession();
             }
         } else {
-            header("location: ".FRONT_ROOT."Home/showLoginView");
+            header("location: " . FRONT_ROOT . "Home/showLoginView");
             Session::DeleteSession();
         }
-        //require_once(VIEWS_PATH . "reviewForm.php");
     }
 
     public function delete($reviewCode)
@@ -60,23 +54,14 @@ class ReviewController{
             if (Session::GetTypeLogged() == "Models\Owner") {
                 $loggedOwner = Session::GetLoggedUser();
                 $result = $this->reviewService->srv_deleteReview($reviewCode, $loggedOwner->getOwnerCode());
-                if($reviewCode == "")
-                {
-
-                }
-                echo "SOYT RESULT :";
-                var_dump($result);
-                if($result["deleted"] == 1)
-                {
+                if ($result["deleted"] == 1) {
                     Session::SetOkMessage("Review Successfully deleted!");
-                    header("location: ".FRONT_ROOT."Keeper/showProfileKeeper/".$result["review"]->getCodeKeeper());
-                }else{
-                    Session::SetBadMessage("Something wrong happen!");
-                    header("location: ".FRONT_ROOT."Keeper/showProfileKeeper/".$result["review"]->getCodeKeeper());
+                    header("location: " . FRONT_ROOT . "Keeper/showProfileKeeper/" . $result["review"]->getCodeKeeper());
+                } else {
+                    Session::SetBadMessage("We cant delete this review.Try later!");
+                    header("location: " . FRONT_ROOT . "Keeper/showProfileKeeper/" . $result["review"]->getCodeKeeper());
                 }
             }
         }
     }
 }
-
-?>
--- DAO/ReviewDAO.php
+++ DAO/ReviewDAO.php
@@ -4,20 +4,20 @@ namespace DAO;
 
 use \Exception as Exception;
 use Models\Review as Review;
-use DAO\QueryType as QueryType;
 use DAO\Connection as Connection;
 
-class ReviewDAO{
+class ReviewDAO
+{
 
     private $tableName = "review";
     private $connection = null;
 
     public function Add(Review $review)
     {
-        try{
-            echo "HOLA TOY EN DAO ?";
-            //Ver como manejo el codeReview si con id_autoincrement o con un codigo como el resto de objs
-            $query = "INSERT INTO ".$this->tableName." (keeperCode,ownerCode,comment,score,reviewCode) VALUES
+        try {
+
+
+            $query = "INSERT INTO " . $this->tableName . " (keeperCode,ownerCode,comment,score,reviewCode) VALUES
             (:codeKeeper,:codeOwner,:comment,:score,:reviewCode);";
 
             $this->connection = Connection::GetInstance();
@@ -27,41 +27,34 @@ class ReviewDAO{
             $parameters["comment"] = $review->getComment();
             $parameters["score"] = $review->getScore();
             $parameters["reviewCode"] = $review->getCodeReview();
-            
-            $resultQ = $this->connection->ExecuteNonQuery($query,$parameters);
 
-            echo "RESULTQ";
-            var_dump($resultQ);
-            if($resultQ == 1)
-            {   
+            $resultQ = $this->connection->ExecuteNonQuery($query, $parameters);
+
+            if ($resultQ == 1) {
                 $this->connection = Connection::GetInstance();
                 $queryTwo = "SELECT updateKeepScoreFunc(:p_keeperCode);";
 
 
                 $parametersTwo["p_keeperCode"] = $review->getCodeKeeper();
 
-                $resultCall = $this->connection->ExecuteNonQuery($queryTwo,$parametersTwo);
+                $resultCall = $this->connection->ExecuteNonQuery($queryTwo, $parametersTwo);
             }
-            echo "RESULT CALL";
-            var_dump($resultCall);
+
             return $resultQ;
-            
-        }catch(Exception $ex)
-        {
+        } catch (Exception $ex) {
             throw $ex;
         }
     }
 
-    //public function canReview()
 
 
-    //Review + info de quien la escribe
+
+    //Review + info
     public function getAllByKeeperCode($keeperCode)
     {
-        try{
+        try {
 
-            //Ver si necesito order by
-            $query = "SELECT r.keeperCode,r.ownerCode,r.comment,r.score,r.timeStamp,r.reviewCode,o.email,o.name,o.lastname,o.pfp FROM ".$this->tableName." as r 
+            $query = "SELECT r.keeperCode,r.ownerCode,r.comment,r.score,r.timeStamp,r.reviewCode,o.email,o.name,o.lastname,o.pfp FROM " . $this->tableName . " as r 
             JOIN owner as o
             ON o.ownerCode = r.ownerCode
             WHERE r.keeperCode = :codeKeeper
@@ -71,12 +64,11 @@ class ReviewDAO{
 
             $parameters["codeKeeper"] = $keeperCode;
 
-            $resultSet = $this->connection->Execute($query,$parameters);
+            $resultSet = $this->connection->Execute($query, $parameters);
 
             $arrayReviews = array();
 
-            foreach($resultSet as $row)
-            {
+            foreach ($resultSet as $row) {
                 $review["keeperCode"] = $row["keeperCode"];
                 $review["ownerCode"] = $row["ownerCode"];
                 $review["comment"] = $row["comment"];
@@ -89,12 +81,11 @@ class ReviewDAO{
                 $review["pfp"] = $row["pfp"];
 
 
-                array_push($arrayReviews,$review);
+                array_push($arrayReviews, $review);
             }
 
             return $arrayReviews;
-        }catch(Exception $ex)
-        {
+        } catch (Exception $ex) {
             throw $ex;
         }
     }
@@ -103,22 +94,20 @@ class ReviewDAO{
     //GetAllByCode con un WHERE si codeOwner = :code OR codeKeeper = :code,total es unico...
     public function getAllByOwnerCode($ownerCode)
     {
-        try{
+        try {
 
-            //Ver si necesito order by
-            $query = "SELECT * FROM ".$this->tableName."
+            $query = "SELECT * FROM " . $this->tableName . "
             WHERE codeOwner = :codeOwner;";
 
             $this->connection = Connection::GetInstance();
 
             $parameters["codeOwner"] = $ownerCode;
 
-            $resultSet = $this->connection->Execute($query,$parameters);
+            $resultSet = $this->connection->Execute($query, $parameters);
 
             $arrayReviews = array();
 
-            foreach($resultSet as $row)
-            {
+            foreach ($resultSet as $row) {
                 $review = new Review();
 
                 $review->setCodeKeeper($row["keeperCode"]);
@@ -128,25 +117,25 @@ class ReviewDAO{
                 $review->setTimeStamp($row["timeStamp"]);
                 $review->setCodeReview($row["reviewCode"]);
 
-                array_push($arrayReviews,$review);
+                array_push($arrayReviews, $review);
             }
 
             return $arrayReviews;
-        }catch(Exception $ex)
-        {
+        } catch (Exception $ex) {
             throw $ex;
         }
     }
 
-    public function canReview($ownerCode,$keeperCode)
+    //checkPrevBooks on status finished & no more than 3 reviews
+    public function canReview($ownerCode, $keeperCode)
     {
-        try{
+        try {
 
             $query = "SELECT COUNT(*) FROM booking
             WHERE ownerCode = :ownerCode AND keeperCode = :keeperCode AND status = :status;";
 
 
-            $queryTwo = "SELECT COUNT(*) FROM ".$this->tableName." 
+            $queryTwo = "SELECT COUNT(*) FROM " . $this->tableName . " 
             WHERE ownerCode = :ownerCode AND keeperCode = :keeperCode;";
             $this->connection = Connection::GetInstance();
 
@@ -154,51 +143,38 @@ class ReviewDAO{
             $parameters["keeperCode"] = $keeperCode;
             $parameters["status"] = "finished";
 
-            $result = $this->connection->Execute($query,$parameters);
+            $result = $this->connection->Execute($query, $parameters);
 
             $parametersTwo["ownerCode"] = $ownerCode;
             $parametersTwo["keeperCode"] = $keeperCode;
 
-            $resultCountReview = $this->connection->Execute($queryTwo,$parametersTwo);
+            $resultCountReview = $this->connection->Execute($queryTwo, $parametersTwo);
 
-            if($result[0][0] >= 1 && $resultCountReview <= 2)
-            {
+            if ($result[0][0] >= 1 && $resultCountReview <= 2) {
                 $result = 1;
             }
             return $result;
-        }catch(Exception $ex)
-        {
+        } catch (Exception $ex) {
             throw $ex;
         }
     }
 
-    public function checkLimitReviews($ownerCode,$keeperCode)
+    public function delete($reviewCode, $ownerCode)
     {
-        try{
+        try {
 
-        }catch(Exception $ex)
-        {
-            throw $ex;
-        }
-    }
-
-    public function delete($reviewCode,$ownerCode)
-    {
-        try{
-
-            $query = "SELECT * FROM ".$this->tableName." 
+            $query = "SELECT * FROM " . $this->tableName . " 
             WHERE reviewCode = :reviewCode AND ownerCode = :ownerCode;";
 
             $this->connection = Connection::GetInstance();
 
             $parameters["reviewCode"] = $reviewCode;
-            $parameters["ownerCode"] =$ownerCode;
+            $parameters["ownerCode"] = $ownerCode;
 
-            $resultSet = $this->connection->Execute($query,$parameters);
+            $resultSet = $this->connection->Execute($query, $parameters);
 
             $review = new Review();
-            foreach($resultSet as $row)
-            {
+            foreach ($resultSet as $row) {
                 $review->setCodeKeeper($row["keeperCode"]);
                 $review->setCodeOwner($row["ownerCode"]);
                 $review->setComment($row["comment"]);
@@ -206,27 +182,17 @@ class ReviewDAO{
                 $review->setTimeStamp($row["timeStamp"]);
                 $review->setCodeReview($row["reviewCode"]);
             }
-            //Parece innecesario el ownerCode pero es una revalidacion que el que borra es el propio user
-            $queryDelete = "DELETE FROM ".$this->tableName." 
+            //Using ownerCode make it safer
+            $queryDelete = "DELETE FROM " . $this->tableName . " 
             WHERE reviewCode = :reviewCode AND ownerCode = :ownerCode ;";
 
-            $result = $this->connection->ExecuteNonQuery($queryDelete,$parameters);
+            $result = $this->connection->ExecuteNonQuery($queryDelete, $parameters);
 
             $arrayResult["review"] = $review;
             $arrayResult["deleted"] = $result;
             return $arrayResult;
-
-            
-        }catch(Exception $ex)
-        {
+        } catch (Exception $ex) {
             throw $ex;
         }
     }
-
-    
-
-
-
 }
-
-?>
