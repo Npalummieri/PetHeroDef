23c0778c2298d4a6d23f14e6fac6ca2144afbe78
Npalummieri <palummierinicolas@gmail.com>
2024-03-28 13:58:34 -0300

    revService minor changes

--- Services/ReviewService.php
+++ Services/ReviewService.php
@@ -1,65 +1,54 @@
-<?php 
+<?php
 
 namespace Services;
 
 use \Exception as Exception;
 use Models\Review as Review;
 use DAO\ReviewDAO as ReviewDAO;
-use Services\BookingService as BookingService;
-use Services\KeeperService as KeeperService;
-class ReviewService{
+
+
+class ReviewService
+{
 
     private $reviewDAO;
-    private $bookingService;
-    private $keeperService;
+
 
     public function __construct()
     {
         $this->reviewDAO = new reviewDAO();
-        $this->bookingService = new BookingService();
-        $this->keeperService = new KeeperService();
     }
 
-    public function generateCode() {
-        // Genera un UUID único
-        $uuid = uniqid('REV', true); // Utiliza 'REV' como prefijo
-    
-        // Devuelve el ownerCode generado
+    public function generateCode()
+    {
+        $uuid = uniqid('REV', true);
         return $uuid;
     }
 
-    public function srv_canReview($ownerCode,$keeperCode)
+    public function srv_canReview($ownerCode, $keeperCode)
     {
-        try
-        {
-                $result = null;
-                if(strpos($keeperCode,"KEP") !== false)
-                {
-                    echo "ENTRO EN EL KEP IF?";
-                    if(strpos($ownerCode,"OWN") !== false)
-                    {
-                        echo "ENTRO EN EL OWN IF?";
-                        $result = $this->reviewDAO->canReview($ownerCode,$keeperCode);
-                    }
-                }
-            
+        try {
+            $result = null;
+            if (strpos($keeperCode, "KEP") !== false) {
 
-            return $result; //Asumimos que puede tomar 0 o +1
-        }catch(Exception $ex)
-        {
+                if (strpos($ownerCode, "OWN") !== false) {
+
+                    $result = $this->reviewDAO->canReview($ownerCode, $keeperCode);
+                }
+            }
+
+
+            return $result; // 0 || +1
+        } catch (Exception $ex) {
             $ex->getMessage();
         }
     }
 
-    public function srv_add($ownerCode,$keeperCode,$comment,$score)
+    public function srv_add($ownerCode, $keeperCode, $comment, $score)
     {
-        $checkPrev = $this->srv_canReview($ownerCode,$keeperCode);
-        echo "SOY CHECKPREV";
-        var_dump($checkPrev);
-        //Deberia verificar que $comment,$score este bien
-        if($checkPrev != null)
-        {
-            //Si todos los datos ya fueron saneados
+        $checkPrev = $this->srv_canReview($ownerCode, $keeperCode);
+
+        if ($checkPrev != null) {
+
             $review = new Review();
             $review->setCodeKeeper($keeperCode);
             $review->setCodeOwner($ownerCode);
@@ -73,38 +62,32 @@ class ReviewService{
         return $resultAdd;
     }
 
-    
+
     public function srv_GetReviews($keeperCode)
     {
-        try{
+        try {
             $arrayReviews = null;
-            if(strpos($keeperCode,"KEP") !== false)
-            {
+            if (strpos($keeperCode, "KEP") !== false) {
                 $arrayReviews = $this->reviewDAO->getAllByKeeperCode($keeperCode);
             }
-            
+
             return $arrayReviews;
-        }catch(Exception $ex)
-        {
+        } catch (Exception $ex) {
             $ex->getMessage();
         }
     }
 
-    public function srv_deleteReview($codeReview,$ownerCodeLog)
+    public function srv_deleteReview($codeReview, $ownerCodeLog)
     {
-        try{
+        try {
 
-            $result = $this->reviewDAO->delete($codeReview,$ownerCodeLog);
-            
+            $result = $this->reviewDAO->delete($codeReview, $ownerCodeLog);
+
             return $result;
-        }catch(Exception $ex)
-        {
+        } catch (Exception $ex) {
             echo $ex->getMessage();
         }
 
         return $result;
     }
-
 }
-
-?>
