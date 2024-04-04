fbd7abd60b40daf4679a1e5e602fabad21a80cbb
Npalummieri <palummierinicolas@gmail.com>
2024-04-04 13:18:14 -0300

    removed email option

--- Views/editProfileOwn.php
+++ Views/editProfileOwn.php
@@ -4,23 +4,21 @@ include("nav.php");
 ?>
 
 
-<section class="section edit-profile-section" id="edit-profile">
-    <div class="container">
-        <h2>Edit Profile</h2>
+<section class="section edit-profile-section text-white  mt-5 m-3"  id="edit-profile">
+    <div class="container rounded" style="background-color: #110257;">
+        <h2 class="text-center">Edit Profile</h2>
         <form action="<?php echo FRONT_ROOT."Owner/updateOwner" ?>" method="post" enctype="multipart/form-data">
-            <div class="form-group">
+        <div class="form-group m-3">
                 <label for="profile-picture">Profile Picture:</label>
                 <input type="file" class="form-control" id="profile-picture" name="pfp">
             </div>
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" class="form-control" id="email" name="email" value="<?php echo $infoOwner->getEmail(); ?>">
-            </div>
-            <div class="form-group">
+            <div class="form-group m-3">
                 <label for="bio">Bio:</label>
-                <textarea class="form-control" id="bio" name="bio"><?php echo "asdasdasd" ?></textarea>
+                <textarea class="form-control" id="bio" name="bio"><?php echo $infoOwner->getBio(); ?></textarea>
             </div>
+            <div class="form-group m-3 p-2 text-end">
             <button type="submit" class="btn btn-primary">Save Changes</button>
+            </div>
         </form>
     </div>
 </section>
