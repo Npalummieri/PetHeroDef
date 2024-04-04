2e8001119cf09ec10e30b3a52ff4b5522a49643e
Npalummieri <palummierinicolas@gmail.com>
2024-04-04 13:14:40 -0300

    styles changed

--- Views/login.php
+++ Views/login.php
@@ -5,26 +5,28 @@
  use Utils\Session as Session;
 ?>
 <div class="wrapper row4">
-<main class="container clear rounded" style="width: max-content;"> 
+<main class="container clear mt-5" style="width: max-content;"> 
   <div class="content"> 
-      <h2>LOGIN</h2>
+      <h2 class="text-center text-white bg-dark rounded">LOGIN</h2>
       <?php  include_once("msgeDisplay.php") ?>
       
-        <form action="<?php echo FRONT_ROOT . "Auth/login" //2 ?>" method="POST" class="login-form p-5 text-white"  style = "background-color: #364a6e;">
-          <div class="form-group">
-            <label for="User">User</label>
-            <input type="text" name="userField" class="form-control form-control-lg" placeholder="Username or Email" required>
+        <form action="<?php echo FRONT_ROOT . "Auth/login" //2 ?>" method="POST" class="login-form p-5 text-white rounded"  style = "background-color: #110257;">
+          <div class="form-group m-2">
+            <label for="user">User</label>
+            <input type="text" name="userField" id="user" class="form-control form-control-lg" placeholder="Username or Email" required>
           </div>
-          <div class="form-group">
-            <label for="">Password</label>
-            <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" required>
+          <div class="form-group m-2">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" class="form-control form-control-lg" placeholder="Password" required>
           </div>
 
-          <div>
+          <div class="m-2">
             <p><a href="<?php echo FRONT_ROOT."Auth/recoverPasswordView" ?>" class="text-white">Forgot password? <strong>CLICK HERE</strong></a></p>
           </div>
           <br/>
+          <div class="m-2">
           <button class="btn btn-dark btn-block btn-lg" type="submit">Login</button>
+          </div>
         </form>
   </div>
 </main>
